3cb98a574946d60cb98168e4760f21270d86c3e7
Amber Taveras <ambertaveras@ambers-mbp.myfiosgateway.com>
2023-01-28 15:41:34 -0500

    adds login and logout features

--- controllers/users.js
+++ controllers/users.js
@@ -16,6 +16,32 @@ router.post('/signup', function (req, res) {
     req.body.password = hashedPassword;
     User.create(req.body, function (error, newUser) {
         req.session.userId = newUser._id;
+        res.redirect('/workouts');
+    });
+});
+
+// login form
+router.get('/login', function (req, res) {
+    res.render('login.ejs');
+});
+
+// handle form submission
+router.post('/login', function (req, res) {
+    User.findOne({
+        email: req.body.email
+    },
+    function (error, foundUser) {
+        if (!foundUser) return res.redirect('/login');
+        const isMatched = bcrypt.compareSync(req.body.password, foundUser.password);
+        if (!isMatched) return res.redirect('/login');
+        req.session.userId = foundUser._id;
+        res.redirect('/workouts');
+    });
+});
+
+// logout
+router.get('/logout', function (req, res) {
+    req.session.destroy(function (error) {
         res.redirect('/');
     });
 });
