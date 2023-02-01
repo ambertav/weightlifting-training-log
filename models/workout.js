06c95de7e0c42b96c391cda20db7dd46c53598d4
Amber Taveras <ambertaveras@ambers-mbp.myfiosgateway.com>
2023-01-31 21:13:50 -0500

    creates workout schema and model

---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    day: {
+        type: String,
+        required: true
+    },
+    exercise: [{
+        name: String,
+        weight: {
+            type: Number,
+            min: 0,
+            required: true
+        },
+        sets: {
+            type: Number,
+            min: 1,
+            required: true
+        },
+        reps: {
+            type: Number,
+            min: 1,
+            required: true
+        }, 
+    }],
+    completed: false
+});
+
+module.exports = mongoose.model('Workout', workoutSchema);
