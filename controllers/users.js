5e555fe73ae87eb1fe28bdef1f177183a226580a
Amber Taveras <taveras.amber@gmail.com>
2023-06-15 17:37:14 -0400

    adds s3 config, requires dotenv, implements photo upload and saves to user instance

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
+require('dotenv').config();
+
+const {
+    S3Client,
+    PutObjectCommand
+} = require('@aws-sdk/client-s3');
+
+const s3Config = {
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+};
+
+const s3Client = new S3Client(s3Config);
+const s3BaseUrl = 'https://s3.us-east-2.amazonaws.com/';
+
 // sign up form
 router.get('/signup', function (req, res) {
     res.render('signup.ejs', {
@@ -64,6 +82,32 @@ router.get('/users/me', function (req, res) {
             user
         });
     });
+});
+
+// user profile photo upload
+router.put('/users/me/photo/edit', async function (req, res) {
+    const file = req.files.profilePhoto;
+    const fileInput = file.name.split('.');
+    const fileType = fileInput[1];
+    const fileName = `${req.session.userId}.${fileType}`
+
+    const bucketParams = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: fileName,
+        Body: file.data,
+    };
+
+    try {
+        await s3Client.send(new PutObjectCommand(bucketParams));
+        User.findById(req.session.userId, function (error, user) {
+            user.profilePhoto = `${s3BaseUrl}${bucketParams.Bucket}/${fileName}`
+            user.save(function () {
+                res.redirect('/users/me');
+            });
+        })
+    } catch (err) {
+        console.log('Error', err);
+    }
 });
 
 // logout
