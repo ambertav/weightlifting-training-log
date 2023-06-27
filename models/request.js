bebd3086f10f8a60a823343250397a69cfc8ea62
Amber Taveras <taveras.amber@gmail.com>
2023-06-26 23:23:58 -0400

    initializes friend request model

---
+++ models/request.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema({
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+
+module.exports = mongoose.model('Request', requestSchema);
