af10df3855ffa555cd147e164fd7e909fbc637e2
Amber Taveras <taveras.amber@gmail.com>
2023-07-21 14:27:08 -0400

    prevents creating multiple requests

--- controllers/requests.js
+++ controllers/requests.js
@@ -5,8 +5,14 @@ const User = require('../models/user');
 
 // create request
 router.post('/users/request', function (req, res) {
-    Request.create(req.body, function (error, newRequest) {
-        res.redirect('/users/me');
+    Request.find({
+        from: {$in: [req.body.from, req.body.to]},
+    }, function (error, request) {
+        if (!request) {
+            Request.create(req.body, function (error, newRequest) {
+                res.redirect('/users/me');
+            });
+        }
     });
 });
 
