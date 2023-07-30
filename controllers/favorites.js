47ee0c25ff70c8744377a5700d5edfc114080ab4
Amber Taveras <taveras.amber@gmail.com>
2023-07-30 14:43:53 -0400

    builds favorite index route

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -4,6 +4,20 @@ const Workout = require('../models/workout');
 const Favorite = require('../models/favorite');
 const Movement = require('../models/movement');
 
+// index
+router.get('/favorites', function (req, res) {
+    Favorite.find({
+        $or: [
+            {createdBy: req.session.userId},
+            {sharedWith: req.session.userId},
+        ]
+    }).populate('exercise.movement')
+    .exec(function (error, favorites) {
+        res.render('favorite/index.ejs', {
+            favorites
+        });
+    })
+})
 
 // create
 router.post('/workouts/:id/favorite', function (req, res) {
