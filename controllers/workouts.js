ccc8c7d615be5f974743b650b9d7f7ba8712f2fb
Amber Taveras <taveras.amber@gmail.com>
2023-08-16 16:22:49 -0400

    updates routes to try catch for better error handling

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -6,37 +6,54 @@ const Movement = require('../models/movement');
 let exercisesArray = [];
 
 // index
-router.get('/workouts', function (req, res) {
-    Workout.find({createdBy: req.session.userId})
-    .populate('exercise.movement')
-    .exec(function (error, allWorkouts) {
+router.get('/workouts', async function (req, res) {
+    try {
+        const allWorkouts = await Workout.find({
+                createdBy: req.session.userId
+            })
+            .populate('exercise.movement')
+            .exec();
+
         res.render('workout/index.ejs', {
             workouts: allWorkouts
         });
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching workouts.');
+    }
 });
 
 // new
-router.get('/workouts/new', function (req, res) {
-    Movement.find({
-        createdBy: {
-            $in: [req.session.userId, null]
-        }
-    }, function (error, allMovements) {
+router.get('/workouts/new', async function (req, res) {
+    try {
+        const availableMovements = await Movement.find({
+                createdBy: {
+                    $in: [req.session.userId, null] // allows search for both default and movements created specifically by user
+                }
+            })
+            .exec();
         res.render('workout/new.ejs', {
-            movements: allMovements
+            movements: availableMovements
         });
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching movements.');
+    }
 });
 
 // delete
-router.delete('/workouts/:id', function (req, res) {
-    Workout.findOneAndDelete({
-        createdBy: req.session.userId,
-        _id: req.params.id
-    }, function (error, deletedWorkout) {
-        res.redirect('/workouts');
-    });
+router.delete('/workouts/:id', async function (req, res) {
+    try {
+        const deletedWorkout = await Workout.findOneAndDelete({
+            createdBy: req.session.userId,
+            _id: req.params.id
+        });
+        if (deletedWorkout) res.redirect('/workouts');
+        else res.status(404).send('Workout not found, could not delete.');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while deleteing the workout.');
+    }
 });
 
 // update
@@ -94,48 +111,70 @@ router.post('/workouts', function (req, res) {
 });
 
 // edit
-router.get('/workouts/:id/edit', function (req, res) {
-    Workout.findById({
-        createdBy: req.session.userId,
-        _id: req.params.id
-    })
-    .populate('exercise.movement')
-    .exec(function (error, foundWorkout) {
-        Movement.find({
-            createdBy: {
-                $in: [req.session.userId, null]
-            }
-        }, function (error, allMovements) {
-            res.render('workout/edit.ejs', {
-                workout: foundWorkout,
-                movements: allMovements
-            });
+router.get('/workouts/:id/edit', async function (req, res) {
+    try {
+        const foundWorkout = await Workout.findById({
+                createdBy: req.session.userId,
+                _id: req.params.id
+            })
+            .populate('exercise.movement')
+            .exec();
+
+        const availableMovements = await Movement.find({
+                createdBy: {
+                    $in: [req.session.userId, null]
+                }
+            })
+            .exec();
+
+        if (foundWorkout) res.render('workout/edit.ejs', {
+            workout: foundWorkout,
+            movements: availableMovements
         });
-    });
+        else res.status(404).send('Workout not found, could not edit.');
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('An error occurred while fetching the workout.');
+    }
 });
 
-// toggle complete and favorite
-router.put('/workouts/:id/complete', function (req, res) {
-    if (req.body.change === 'isComplete') {
-        Workout.findById({
-            createdBy: req.session.userId,
-            _id: req.body.id
-        }, function (error, workout) {
-            workout.isComplete = !workout.isComplete;
-            workout.save();
-        });
+// toggle complete
+router.put('/workouts/:id/complete', async function (req, res) {
+    try {
+        if (req.body.change === 'isComplete') {
+            const workout = await Workout.findOne({
+                createdBy: req.session.userId,
+                _id: req.body.id
+            });
+
+            if (workout) {
+                workout.isComplete = !workout.isComplete;
+                await workout.save();
+                res.sendStatus(200);
+            } else {
+                res.status(404).send('Workout not found.');
+            }
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('An error occurred while updating workout status.');
     }
-})
+});
 
 // show
-router.get('/workouts/:id', function (req, res) {
-    Workout.findById(req.params.id)
-    .populate('exercise.movement')
-    .exec(function (error, foundWorkout) {
+router.get('/workouts/:id', async function (req, res) {
+    try {
+        const foundWorkout = await Workout.findById(req.params.id)
+            .populate('exercise.movement')
+            .exec();
+
         res.render('workout/show.ejs', {
             workout: foundWorkout
         });
-    });
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('An error occurred while fetching the workout.');
+    }
 });
 
 module.exports = router;
