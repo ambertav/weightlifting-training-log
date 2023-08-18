1c2db630f71f0995d6ab1a36f72e02331551d391
Amber Taveras <taveras.amber@gmail.com>
2023-08-18 11:45:25 -0400

    utilizes async/await try/catch for better error handling

--- controllers/requests.js
+++ controllers/requests.js
@@ -4,33 +4,45 @@ const Request = require('../models/request');
 const User = require('../models/user');
 
 // create request
-router.post('/users/request', function (req, res) {
-    Request.findOne({
-        from: {$in: [req.body.from, req.body.to]},
-        to: {$in: [req.body.from, req.body.to]}
-    }, function (error, request) {
-        if (!request) {
-            Request.create(req.body, function (error, newRequest) {
-                res.redirect('/users/me');
-            });
-        }
-    });
+router.post('/users/request', async function (req, res) {
+    try {
+        // checks if request exists
+        const request = await Request.findOne({
+            from: {$in: [req.body.from, req.body.to]},
+            to: {$in: [req.body.from, req.body.to]}
+        })
+        .exec();
+        
+        // creates a request if none exists
+        if (!request) await Request.create(req.body);
+
+        res.redirect('/users/me');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while creating the friend request.');
+    }
 });
 
 // update request
-router.put('/users/request/edit', function (req, res) {
-    Request.findById(req.body.requestId, function (error, foundRequest) {
+router.put('/users/request/edit', async function (req, res) {
+    try {
+        const foundRequest = await Request.findById(req.body.requestId);
+        if (!foundRequest) return res.status(404).send('Friend request not found.');
+
+        // if user accepts, update status and save
         if (req.body.decision === 'Accept') {
-            foundRequest.status = 'accepted'
-            foundRequest.save().then(function () {
-                res.redirect('/users/me')
-            });
-        } else {
-            Request.findByIdAndDelete(foundRequest._id, function (error, deletedRequest) {
-                res.redirect('/users/me');
-            });
+            foundRequest.status = 'accepted';
+            await foundRequest.save();
         }
-    });
+
+        // if user declined, delete request
+        else await Request.findByIdAndDelete(foundRequest._id);
+        
+        res.redirect('/users/me')
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while updating the friend request.')
+    }
 });
 
 
