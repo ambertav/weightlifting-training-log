2f33ca884f90fc2f66b5d019e63215ee609f0ba2
Amber Taveras <taveras.amber@gmail.com>
2023-08-18 12:53:24 -0400

    refactors to use async/await, try/catch, removes duplicate logic

--- controllers/movements.js
+++ controllers/movements.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const Movement = require('../models/movement');
 
 const muscleGroups = ['Deltoids', 'Triceps', 'Biceps', 'Forearms', 'Chest', 'Abdominals', 'Upper Back', 'Lower Back', 'Glutes', 'Quadriceps', 'Hamstrings', 'Calves'];
-let muscleArray = [];
 
 // index
-router.get('/movements', function (req, res) {
-    Movement.find({
-        createdBy: {
-            $in: [req.session.userId, null]
-        }
-    }, function (error, allMovements) {
+router.get('/movements', async function (req, res) {
+    try {
+        const allMovements = await Movement.find({
+            createdBy: {
+                $in: [req.session.userId, null] // allows access both default and movements created specifically by user
+            }
+        });
+
         res.render('movement/index.ejs', {
             movements: allMovements
         });
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching movements.');
+    }
 });
 
 // new
@@ -26,66 +30,86 @@ router.get('/movements/new', function (req, res) {
 });
 
 // delete
-router.delete('/movements/:id', function (req, res) {
-    Movement.findOneAndDelete({
-        createdBy: req.session.userId,
-        _id: req.params.id
-    }, function (error, data) {
+router.delete('/movements/:id', async function (req, res) {
+    try {
+        const deletedMovement = await Movement.findOneAndDelete({
+            createdBy: req.session.userId,
+            _id: req.params.id
+        });
+
         res.redirect('/movements');
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while deleting the movement.');
+    }
 });
 
 // update
-router.put('/movements/:id', function (req, res) {
-    muscleArray = [];
-    for (const key of Object.keys(req.body.musclesWorked)) {
-        muscleGroups.forEach(function (muscle) {
-            if (key === muscle) {
-                muscleArray.push(muscle)
-            }
+router.put('/movements/:id', async function (req, res) {
+    try {
+        const editMovement = formatMovementData(req.body, req.session.userId); // format req.body per schema
+
+        const updatedMovement = await Movement.findOneAndUpdate({
+            createdBy: req.session.userId,
+            _id: req.params.id
+        }, editMovement, {
+            new: true
         });
-    }
-    req.body.musclesWorked = muscleArray;
-    req.body.isCardio = req.body.isCardio === 'on' ? true : false;
-    req.body.isWeighted = req.body.isWeighted === 'on' ? true : false;
-    Movement.findOneAndUpdate({
-        createdBy: req.session.userId,
-        _id: req.params.id
-    }, req.body, {
-        new: true
-    }, function (error, updatedMovement) {
+
         res.redirect('/movements');
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while updating the movement.');
+    }
 });
 
 // create
-router.post('/movements', function (req, res) {
-    muscleArray = [];
-    for (const key of Object.keys(req.body.musclesWorked)) {
-        muscleGroups.forEach(function (muscle) {
-            if (key === muscle) {
-                muscleArray.push(muscle)
-            }
-        });
-    }
-    req.body.musclesWorked = muscleArray;
-    req.body.isCardio = req.body.isCardio === 'on' ? true : false;
-    req.body.isWeighted = req.body.isWeighted === 'on' ? true : false;
-    req.body.createdBy = req.session.userId;
-    Movement.create(req.body, function (error, createdMovement) {
+router.post('/movements', async function (req, res) {
+    try {
+        const newMovement = formatMovementData(req.body, req.session.userId); // format req.body per schema
+
+        const createdMovement = await Movement.create(newMovement);
+
         res.redirect('/movements');
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while creating the movement.');
+    }
 });
 
 // edit
-router.get('/movements/:id/edit', function (req, res) {
-    Movement.findById(req.params.id, function (error, foundMovement) {
+router.get('/movements/:id/edit', async function (req, res) {
+    try {
+        const foundMovement = await Movement.findById(req.params.id);
         res.render('movement/edit.ejs', {
             movement: foundMovement,
             muscleGroups
         });
-    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching the movement.');
+    }
 });
+
+// format the movement data from req.body
+function formatMovementData(movementData, userId) {
+    const selectedMuscles = Object.keys(movementData.musclesWorked).filter(function (key) {
+        return muscleGroups.includes(key);
+    });
+    const musclesWorked = [...selectedMuscles];
+
+    const isCardio = movementData.isCardio === 'on' ? true : false;
+    const isWeighted = movementData.isWeighted === 'on' ? true : false;
+    const createdBy = userId;
+
+    return {
+        ...movementData,
+        musclesWorked,
+        isCardio,
+        isWeighted,
+        createdBy,
+    };
+}
 
 
 module.exports = router;
