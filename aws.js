074c57f419c22e4cd2cb1e345252bfef5771e2a7
Amber Taveras <taveras.amber@gmail.com>
2023-08-23 15:44:14 -0400

    separates aws s3 info for better manageability

---
+++ aws.js
@@ -0,0 +1,19 @@
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+require('dotenv').config();
+
+const s3Config = {
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+};
+
+const s3Client = new S3Client(s3Config);
+const s3BaseUrl = 'https://s3.us-east-2.amazonaws.com/';
+
+module.exports = {
+    s3Client,
+    s3BaseUrl,
+    PutObjectCommand
+};
