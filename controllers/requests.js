07706f8face1ab203c59c0b121def462bef3c4a6
Amber Taveras <taveras.amber@gmail.com>
2023-11-22 02:16:58 -0500

    updates error handling in request controller

--- controllers/requests.js
+++ controllers/requests.js
@@ -1,49 +1,55 @@
+const mongoose = require('mongoose');
 const Request = require('../models/request');
 
 
 // create request
 async function createRequest (req, res) {
     try {
-        // checks if request exists
-        const request = await Request.findOne({
+        // verifies that both associated users have valid ids
+        if (!mongoose.Types.ObjectId.isValid(req.body.from) || !mongoose.Types.ObjectId.isValid(req.body.to)) {
+            return res.status(400).json({ error: 'Requests can only be made between valid users', reload: true });
+        }
+
+        // checks if request exists with count
+        const count = await Request.countDocuments({
             from: {$in: [req.body.from, req.body.to]},
             to: {$in: [req.body.from, req.body.to]}
-        })
-        .lean();
+        });
+        
+        // returns error if request exists
+        if (count) return res.status(409).json({ error: 'Duplicate request', reload: true });
         
         // creates a request if none exists
-        if (!request) await Request.create(req.body);
+        else await Request.create(req.body);
 
         res.redirect('/users/me');
     } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred while creating the friend request.');
+        res.status(500).send('An error occurred while creating the friend request');
     }
 }
 
 // update request
 async function handleRequest (req, res) {
     try {
-        const foundRequest = await Request.findById(req.body.requestId)
-        if (!foundRequest) return res.status(404).send('Friend request not found.');
+        // search by request id and userId to ensure request exists and the logged in user is authorized to make decision
+        const foundRequest = await Request.findOne({ _id: req.body.requestId, to: req.session.userId });
+        // else return error
+        if (!foundRequest) return res.status(404).json({ error: 'Friend request not found', reload: true });
 
         // if user accepts, update status and save
         if (req.body.decision === 'Accept') {
             foundRequest.status = 'accepted';
             await foundRequest.save();
         }
-
-        // if user declined, delete request
-        else await Request.findByIdAndDelete(foundRequest._id);
+        // if user decline, delete request
+        else if (req.body.decision === 'Decline') await Request.findByIdAndDelete(foundRequest._id);
+        else return res.status(400).json({ error: 'Invalid decision for status update', reload: true });
         
-        res.redirect('/users/me')
+        res.redirect('/users/me');
     } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred while updating the friend request.')
+        res.status(500).send('An error occurred while updating the friend request');
     }
 }
 
 
-module.exports = { 
-    createRequest, handleRequest,
-}
+module.exports = {  createRequest, handleRequest }
